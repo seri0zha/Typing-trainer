65b2a98e70924f39159bfdcb2b82fa4b5f3689c6
seri0zha <ise200017@gmail.com>
2021-06-12 19:34:04 +0300

    refactor: Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import styled from "styled-components";
 import Header from "./components/Header";
 import Trainer from "./components/Trainer";
-import Stats from "./components/Header/Stats";
-import { useAppSelector } from "./store";
 
 const AppWrapper = styled.div`
   color: #ddd;
